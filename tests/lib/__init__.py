21d8607099cb0afa458bbfeb4198217f06050870
q0w <43147888+q0w@users.noreply.github.com>
2022-03-09 05:28:00 +0300

    Use subprocess

--- tests/lib/__init__.py
+++ tests/lib/__init__.py
@@ -1034,7 +1034,7 @@ def _create_test_package(
 def _create_svn_repo(repo_path: Path, version_pkg_path: str) -> str:
     repo_url = path_to_url(repo_path / "pip-test-package-repo" / "trunk")
     subprocess.check_call(
-        make_command("svnadmin", "create", "pip-test-package-repo"), cwd=repo_path
+        "svnadmin create pip-test-package-repo".split(), cwd=repo_path
     )
     Subversion.run_command(
         make_command(
